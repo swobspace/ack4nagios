e50f3cc518bb7ea556177c6e65c022957450809e
Wolfgang Barth <wob@swobspace.net>
2014-07-11 16:30:08 +0200

    validate connection_type with pattern

--- app/models/site.rb
+++ app/models/site.rb
@@ -6,7 +6,8 @@ class Site < ActiveRecord::Base
 
   # -- validations and callbacks
   validates :name, presence: true, uniqueness: true
-  validates :connection_type, :uri, presence: true
+  validates :connection_type, format: { with: /\A(unix:\/\/)\z/}
+  validates :uri, presence: true
 
   def to_s
     "#{connection_type}#{uri}"
