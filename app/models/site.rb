31bf7448ce70518dd417628424d629f47cd5b356
Wolfgang Barth <wob@swobspace.net>
2014-07-11 13:39:24 +0200

    add validations to site

--- app/models/site.rb
+++ app/models/site.rb
@@ -1,2 +1,16 @@
 class Site < ActiveRecord::Base
+  # -- associations
+
+  # -- configuration
+  CONNECTION_TYPES = ['unix://']
+
+  # -- validations and callbacks
+  validates :name, presence: true, uniqueness: true
+  validates :connection_type, :uri, presence: true
+
+  def to_s
+    "#{connection_type}#{uri}"
+  end
+
+
 end
--- spec/models/site_spec.rb
+++ spec/models/site_spec.rb
@@ -1,5 +1,16 @@
 require 'rails_helper'
 
 RSpec.describe Site, :type => :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { is_expected.to validate_presence_of(:name) }
+  it { is_expected.to validate_presence_of(:connection_type) }
+  it { is_expected.to validate_presence_of(:uri) }
+
+  it "should get plain factory working" do
+    f = FactoryGirl.create(:site)
+    g = FactoryGirl.create(:site)
+    expect(f).to be_valid
+    expect(g).to be_valid
+
+    is_expected.to validate_uniqueness_of :name
+  end
 end
