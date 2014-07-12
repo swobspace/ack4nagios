fbbbe0fbeaa44a35c9977f580a009167c1546926
Wolfgang Barth <wob@swobspace.net>
2014-07-12 11:45:51 +0200

    use model Acknowledge instead of direct Livestatus access

--- app/controllers/acknowledges_controller.rb
+++ app/controllers/acknowledges_controller.rb
@@ -14,24 +14,7 @@ class AcknowledgesController < ApplicationController
   end
 
   def find_acknowledges
-    @acknowledges = connection.get(Livestatus::Service, options)
-  end
-
-  def connection
-    @connection ||= Livestatus::Connection.new({uri: @site.path})
-  end
-
-  def options
-    { 
-      columns: 'host_name description state plugin_output',
-      filter: [
-        "host_state = 0",
-        "host_state_type = 1",
-        "state > 0",
-        "state_type = 1",
-        "acknowledged = 0"
-      ]
-    }
+    @acknowledges = Acknowledge.find(site: @site)
   end
 
 end
