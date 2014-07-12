83ee049a21fee33cb1af700bb331dd9ec09e9572
Wolfgang Barth <wob@swobspace.net>
2014-07-12 17:13:53 +0200

    add tts_active_class for ottrick navis

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -7,4 +7,10 @@ module ApplicationHelper
       end
     end
 
+    def tts_active_class
+      if Ack4nagios::TTS_CONTROLLER.include?(controller.controller_name.to_sym)
+        "active"
+      end
+    end
+
 end
