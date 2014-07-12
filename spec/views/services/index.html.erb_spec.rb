901a38a90a156d5b714aa3fa68ef0af8c3eeadba
Wolfgang Barth <wob@swobspace.net>
2014-07-12 14:03:09 +0200

    check for site name

--- spec/views/services/index.html.erb_spec.rb
+++ spec/views/services/index.html.erb_spec.rb
@@ -2,15 +2,15 @@ require 'rails_helper'
 
 RSpec.describe "services/index", :type => :view do
   before(:each) do
-    site = FactoryGirl.create(:site)
+    @site = FactoryGirl.create(:site)
     assign(:services, [
       Service.create!(
-        :site => site,
+        :site => @site,
         :host => "Host",
         :service_description => "Service Description"
       ),
       Service.create!(
-        :site => site,
+        :site => @site,
         :host => "Host",
         :service_description => "Service Description"
       )
@@ -19,7 +19,7 @@ RSpec.describe "services/index", :type => :view do
 
   it "renders a list of services" do
     render
-    assert_select "tr>td", :text => "Site".to_s, :count => 2
+    assert_select "tr>td", :text => @site.name, :count => 2
     assert_select "tr>td", :text => "Host".to_s, :count => 2
     assert_select "tr>td", :text => "Service Description".to_s, :count => 2
   end
