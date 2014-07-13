7b535d75fe7668e4c700d647cbba44cf1bb5668b
Wolfgang Barth <wob@swobspace.net>
2014-07-13 11:53:46 +0200

    get sticky stuff from args

--- app/models/concerns/service_nagios_acknowledges.rb
+++ app/models/concerns/service_nagios_acknowledges.rb
@@ -15,10 +15,10 @@ private
   # 40 ACKNOWLEDGE_SVC_PROBLEM;<host_name>;<service_description>;<sticky>;<notify>;<persistent>;<author>;<comment>
   def command(args)
     comment = args.fetch(:comment)
-    sticky  = 0
-    notify  = 0
-    author  = 0
-    persistent = 0
+    sticky  = args.fetch(:sticky, 0)
+    notify  = args.fetch(:notify, 0)
+    author  = args.fetch(:author, 0)
+    persistent = args.fetch(:persistent, 0)
 
     ["ACKNOWLEDGE_SVC_PROBLEM", host, service_description, sticky, 
       notify, persistent, author, comment].join(';').to_s
