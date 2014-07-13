802f1ed27f3afe0a1f6e2c48d45e04448778ba46
Wolfgang Barth <wob@swobspace.net>
2014-07-13 18:44:54 +0200

    configurable capistrano

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,14 +1,16 @@
 # config valid only for Capistrano 3.1
 lock '3.2.1'
 
-set :application, 'my_app_name'
-set :repo_url, 'git@example.com:me/my_repo.git'
+config = YAML.load_file('config/deploy-config.yml') || {}
+
+set :application, 'ack4nagios'
+set :repo_url, config['repo_url']
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
 
 # Default deploy_to directory is /var/www/my_app
-# set :deploy_to, '/var/www/my_app'
+set :deploy_to, config['deploy_to']
 
 # Default value for :scm is :git
 # set :scm, :git
@@ -20,16 +22,18 @@ set :repo_url, 'git@example.com:me/my_repo.git'
 # set :log_level, :debug
 
 # Default value for :pty is false
-# set :pty, true
+# set :pty, false
 
 # Default value for :linked_files is []
-# set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/application.yml config/GenericTicketConnector.wsdl}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
+
+set :shell, "bash -l"
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
