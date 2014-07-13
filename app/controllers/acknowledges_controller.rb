17798b130e5c2bdd260b7773b655e001bed6266e
Wolfgang Barth <wob@swobspace.net>
2014-07-13 10:27:52 +0200

    prepare for actions and respect filters

--- app/controllers/acknowledges_controller.rb
+++ app/controllers/acknowledges_controller.rb
@@ -4,11 +4,21 @@ class AcknowledgesController < ApplicationController
 
   # GET /acknowledges
   def index
+    @filter = params[:filter]
     respond_with(@acknowledges)
   end
 
   def create
-    @params = params
+    if service_ids.empty?
+      redirect_to filter_params.merge(action: :index)
+    end
+    if commit == 'Ticket'
+    elsif commit == 'Mail'
+    elsif commit == 'Acknowledge'
+    elsif commit == 'Test'
+      @params = acknowledge_params.merge(commit: commit)
+      render :create
+    end
   end
 
   private
@@ -18,7 +28,34 @@ class AcknowledgesController < ApplicationController
   end
 
   def find_acknowledges
-    @acknowledges = Acknowledge.find(site: @site)
+    @acknowledges = Acknowledge.find(site: @site, filter: filter)
   end
 
+  def acknowledge_params
+    params.permit(:site, :comment, {service_ids: []})
+  end
+
+  def filter_params
+    params.permit(:site, :filter)
+  end
+
+  def service_ids
+    params[:service_ids] || []
+  end
+
+  # Syntax for livestatus filters:
+  # field_tag1:field_value1;...;field_tagN:field_valueN
+  # result: [ "field_tag1 = field_value1", ..., "fieldfield_tagN = field_valueN" ]
+  #
+  def filter
+    if params[:filter].blank?
+      []
+    else
+      params[:filter].gsub(/:/, " = ").split(/;/)
+    end
+  end
+
+  def commit
+    params[:commit]
+  end
 end
