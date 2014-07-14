ee70b1e74dfb48deee95884aca5954dbc6e97caf
Wolfgang Barth <wob@swobspace.net>
2014-07-14 19:03:20 +0200

    add dataTable stuff

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -4,4 +4,8 @@
 //= require turbolinks
 //= require ottrick/ottrick
 //= require bootstrap
+//= require dataTables/jquery.dataTables
+//= require dataTables/bootstrap/3/jquery.dataTables.bootstrap
+//= require dataTables/extras/dataTables.tableTools
+//= require ack4nagios
 //
