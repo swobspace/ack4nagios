741abe837969fe4065b48069c6c323abde59ef9f
Wolfgang Barth <wob@swobspace.net>
2014-07-19 15:37:04 +0200

    link some shared dirs, restart passenger

--- config/deploy.rb
+++ config/deploy.rb
@@ -30,6 +30,7 @@ set :linked_files, %w{config/database.yml config/application.yml config/GenericT
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_dirs, %w{bin log files tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
 
 # Default value for default_env is {}
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
@@ -45,8 +46,7 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
+      execute :touch, release_path.join('tmp/restart.txt')
     end
   end
 
@@ -54,9 +54,8 @@ namespace :deploy do
 
   after :restart, :clear_cache do
     on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
       # within release_path do
-      #   execute :rake, 'cache:clear'
+        # execute :rake, 'cache:clear'
       # end
     end
   end
