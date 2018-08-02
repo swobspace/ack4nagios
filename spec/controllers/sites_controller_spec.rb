85b7a843eb7594cdab44cb7075de8f737560e485
Wolfgang Barth <wob@swobspace.net>
2018-08-02 17:44:42 +0200

    update controller specs to rails5 style

--- spec/controllers/sites_controller_spec.rb
+++ spec/controllers/sites_controller_spec.rb
@@ -39,7 +39,7 @@ RSpec.describe SitesController, :type => :controller do
   describe "GET index" do
     it "assigns all sites as @sites" do
       site = Site.create! valid_attributes
-      get :index, {}, valid_session
+      get :index, params: {}, session: valid_session
       expect(assigns(:sites)).to eq([site])
     end
   end
@@ -47,14 +47,14 @@ RSpec.describe SitesController, :type => :controller do
   describe "GET show" do
     it "assigns the requested site as @site" do
       site = Site.create! valid_attributes
-      get :show, {:id => site.to_param}, valid_session
+      get :show, params: {:id => site.to_param}, session: valid_session
       expect(assigns(:site)).to eq(site)
     end
   end
 
   describe "GET new" do
     it "assigns a new site as @site" do
-      get :new, {}, valid_session
+      get :new, params: {}, session: valid_session
       expect(assigns(:site)).to be_a_new(Site)
     end
   end
@@ -62,7 +62,7 @@ RSpec.describe SitesController, :type => :controller do
   describe "GET edit" do
     it "assigns the requested site as @site" do
       site = Site.create! valid_attributes
-      get :edit, {:id => site.to_param}, valid_session
+      get :edit, params: {:id => site.to_param}, session: valid_session
       expect(assigns(:site)).to eq(site)
     end
   end
@@ -71,30 +71,30 @@ RSpec.describe SitesController, :type => :controller do
     describe "with valid params" do
       it "creates a new Site" do
         expect {
-          post :create, {:site => valid_attributes}, valid_session
+          post :create, params: {:site => valid_attributes}, session: valid_session
         }.to change(Site, :count).by(1)
       end
 
       it "assigns a newly created site as @site" do
-        post :create, {:site => valid_attributes}, valid_session
+        post :create, params: {:site => valid_attributes}, session: valid_session
         expect(assigns(:site)).to be_a(Site)
         expect(assigns(:site)).to be_persisted
       end
 
       it "redirects to the created site" do
-        post :create, {:site => valid_attributes}, valid_session
+        post :create, params: {:site => valid_attributes}, session: valid_session
         expect(response).to redirect_to(Site.last)
       end
     end
 
     describe "with invalid params" do
       it "assigns a newly created but unsaved site as @site" do
-        post :create, {:site => invalid_attributes}, valid_session
+        post :create, params: {:site => invalid_attributes}, session: valid_session
         expect(assigns(:site)).to be_a_new(Site)
       end
 
       it "re-renders the 'new' template" do
-        post :create, {:site => invalid_attributes}, valid_session
+        post :create, params: {:site => invalid_attributes}, session: valid_session
         expect(response).to render_template("new")
       end
     end
@@ -108,20 +108,20 @@ RSpec.describe SitesController, :type => :controller do
 
       it "updates the requested site" do
         site = Site.create! valid_attributes
-        put :update, {:id => site.to_param, :site => new_attributes}, valid_session
+        put :update, params: {:id => site.to_param, :site => new_attributes}, session: valid_session
         site.reload
         expect(site.uri).to be == "/from/anywhere"
       end
 
       it "assigns the requested site as @site" do
         site = Site.create! valid_attributes
-        put :update, {:id => site.to_param, :site => valid_attributes}, valid_session
+        put :update, params: {:id => site.to_param, :site => valid_attributes}, session: valid_session
         expect(assigns(:site)).to eq(site)
       end
 
       it "redirects to the site" do
         site = Site.create! valid_attributes
-        put :update, {:id => site.to_param, :site => valid_attributes}, valid_session
+        put :update, params: {:id => site.to_param, :site => valid_attributes}, session: valid_session
         expect(response).to redirect_to(site)
       end
     end
@@ -129,13 +129,13 @@ RSpec.describe SitesController, :type => :controller do
     describe "with invalid params" do
       it "assigns the site as @site" do
         site = Site.create! valid_attributes
-        put :update, {:id => site.to_param, :site => invalid_attributes}, valid_session
+        put :update, params: {:id => site.to_param, :site => invalid_attributes}, session: valid_session
         expect(assigns(:site)).to eq(site)
       end
 
       it "re-renders the 'edit' template" do
         site = Site.create! valid_attributes
-        put :update, {:id => site.to_param, :site => invalid_attributes}, valid_session
+        put :update, params: {:id => site.to_param, :site => invalid_attributes}, session: valid_session
         expect(response).to render_template("edit")
       end
     end
@@ -145,13 +145,13 @@ RSpec.describe SitesController, :type => :controller do
     it "destroys the requested site" do
       site = Site.create! valid_attributes
       expect {
-        delete :destroy, {:id => site.to_param}, valid_session
+        delete :destroy, params: {:id => site.to_param}, session: valid_session
       }.to change(Site, :count).by(-1)
     end
 
     it "redirects to the sites list" do
       site = Site.create! valid_attributes
-      delete :destroy, {:id => site.to_param}, valid_session
+      delete :destroy, params: {:id => site.to_param}, session: valid_session
       expect(response).to redirect_to(sites_url)
     end
   end
